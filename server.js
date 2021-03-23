df2bb175dbd7f2e0d4cd8be530a45acd7adaae26
Hamish Isaacs <hamisaacs@gmail.com>
2021-03-23 19:17:52 +0800

    Updated server..js and seed.js removed duplication

--- server.js
+++ server.js
@@ -2,22 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 require('dotenv').config()
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-
-const PORT = process.env.PORT || 3000;
-app.use(logger("dev"));
-app.use(express.urlencoded({
-    extended: true
-}));
-
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// morgon logs each action we take on server
+app.use(logger("dev"));
+
+// allow access to public folder when it runs on server(localhost)
 app.use(express.static("public"));
+
+// htmlroutes
+app.use(require("./routes/htmlRoutes"));
+
+// api routes
+app.use(require("./routes/apiRoutes.js"));
+
 mongoose.connect(process.env.MONGODB_DSN, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
